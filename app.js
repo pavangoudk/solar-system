74730d5bfdb5b20a4e140d69e1bbca3f152e7b95
k pavan kumar <pavan.kgoud@gmail.com>
2026-09-07 16:29:28 +0530

    hard coded the db uri in app.js

--- app.js
+++ app.js
@@ -12,10 +12,16 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // user: process.env.MONGO_USERNAME,
+    // pass: process.env.MONGO_PASSWORD,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+
+  user: process.env.MONGO_USERNAME,
+  pass: process.env.MONGO_PASSWORD,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+
 }, function(err) {
     if (err) {
         console.log("error!! " + err)
